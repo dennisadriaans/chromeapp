758e83a3469d5d43ae1b2c83ace4457bc0642aeb
Dennis Adriaansen <adriaansendennis@gmail.com>
2015-06-18 14:32:27 +0200

    add comments

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ app.config(function ($interpolateProvider,  $routeProvider, $locationProvider) {
 
 app.controller('MyController', function($scope, $mdSidenav, $http) {
 
+    //set chrome app always on top
     chrome.app.window.current().isAlwaysOnTop();
 
     document.cookie = 'auth_tkt=4eef912128e7231653f8b8b97e7b61e3620ea77e1674d4116decbabb55008439e6ecafa733753e0c2a565d5d9ccd4c64c5542adf107cf3df5f504b7c854b5eae552423e04'
